5def510cb764775648b8b7eeed01e585a84c7a17
zifelok <zifelok@yahoo.com>
2017-09-21 16:56:01 +0300

    Add EmployeesList

---
+++ EmployeeListTest.Main/ClientApp/components/Create.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import 'isomorphic-fetch';
+
+interface CreateState {
+
+}
+
+export class Create extends React.Component<RouteComponentProps<{}>, CreateState> {
+    constructor() {
+        super();
+
+    }
+
+    public render() {
+        return <div/>;
+    }
+
+    
+}
--- EmployeeListTest.Main/ClientApp/routes.tsx
+++ EmployeeListTest.Main/ClientApp/routes.tsx
@@ -2,11 +2,9 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import { Create } from './components/Create'
 
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
-    <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata' component={ FetchData } />
+    <Route path='/create' component={ Create } />
 </Layout>;
